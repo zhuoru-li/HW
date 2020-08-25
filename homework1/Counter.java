c992c935f745196ede776a8759883a182cf5f1bd
Zhuoru Li <zhuoruli@Zhuorus-MacBook-Pro.local>
2020-08-24 22:42:01 -0700

    Counter class

--- homework1/Counter.java
+++ homework1/Counter.java
@@ -1,2 +1,23 @@
-package homework1;public class Counter {
+package homework1;
+
+public class Counter {
+    private int counter = 0;
+
+    public synchronized void increase() {
+        System.out.println(Thread.currentThread().getName() + " " + counter++);
+    }
+
+    public static void main(String[] args) {
+        Counter c = new Counter();
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                c.increase();
+            }
+        }).start();
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                c.increase();
+            }
+        }).start();
+    }
 }
